229621f4f8acde2c7daf00d4497306d89fe092ce
Vito <vitocarbone94@gmail.com>
2022-12-06 17:24:49 +0100

    A

---
+++ index.js
@@ -0,0 +1,49 @@
+/* ESERCIZIO 1*/
+
+let num1 =x
+let num2= y
+if(num1>num2){
+    console.log(num1 +"è maggiore di "+num2);
+}
+else if(num1<num2){
+    console.log(num1+ "è minore di "+num2);
+} 
+
+/*ESERCIZIO 2*/
+const x= 5;
+if (numero fornito != x){
+    console.log("not equal")
+}
+
+ /*ESERCIZIO 3*/ 
+const pluto = 5;
+let pippo =x;
+if (pippo%5!){
+    console.log("divisibile per 5")
+}
+/* ESERCIZIO 4*/
+const pippo =x
+const pluto =y;
+if (pippo ==8){
+    console.log("Numero uguale a 8");
+}
+else if (pippo + pluto ==8)||(pippo - pluto ==8){
+    console.log("È possibile")
+} 
+else {
+    console.log("Non è possibile")
+}
+
+/*ESERCIZIO 5 */
+let totalShoppingCart= x
+const spedizione = 10  
+if (x>=50){ 
+    console.log("La spedizione è gratuita ");
+    } 
+    else if{ (x<=50)
+    }
+
+/* ESERCIZIO 8*/
+let 
+
+
